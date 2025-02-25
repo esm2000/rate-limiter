ebda2da3fb2197aebe2c8dfbdfe192381b31f579
Emeka Mbazor <embazor@gmail.com>
2025-02-25 01:24:23 -0800

    create tables to track services and users

--- database/init.sql
+++ database/init.sql
@@ -6,3 +6,20 @@ CREATE TABLE rules (
     requests_present INTEGER,
     algorithm VARCHAR
 );
+
+CREATE TABLE services (
+    id UUID PRIMARY KEY,
+    name VARCHAR,
+    creation_time TIMESTAMP,
+    api_key_expiration_time TIMESTAMP,
+    api_key_hash VARCHAR
+);
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    service_id UUID,
+    is_admin BOOLEAN,
+    creation_time TIMESTAMP,
+    password_hash VARCHAR,
+    FOREIGN KEY (service_id) REFERENCES services(id) ON DELETE CASCADE
+);
